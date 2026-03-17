1eb2ae7efd037f7c8af2c64d9f9313b9f7f6e598
eguo <sekasekamenmen@gmail.com>
2026-03-17 21:28:01 +0900

    Updates

---
+++ _next/static/ZYRfpvyva8pvuVMz3npEf/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/swingrecorder-web//_next/:path+",
+        "destination": "/swingrecorder-web/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
